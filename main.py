fd0c187fcc7bc6ab6c4a4472074ba0230672be9d
Trishan <sanjanatrishan@gmail.com>
2025-10-25 00:40:13 +0530

    Add initial FastAPI application with product endpoints

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from models import Product
+app = FastAPI()
+
+products : list= [
+    Product(id=1, name="Laptop", description="A high-end laptop", price=1500.00, in_stock=True),
+    Product(id=2, name="Smartphone", description="A latest model smartphone", price=800.00, in_stock=True),
+    Product(id=3, name="Headphones", description="Noise-cancelling headphones", price=200.00, in_stock=False),
+    Product(id=4, name="Monitor", description="4K UHD Monitor", price=400.00, in_stock=True),
+]
+
+@app.get("/products")
+def get_all_products():
+    return products
+
+@app.get("/products/{product_id}")
+def get_product_by_id(product_id: int):
+    for product in products:
+        if product.id == product_id:
+            print(f"Product found : {product}")
+            return product
+    return {"error":"Product not found"}
