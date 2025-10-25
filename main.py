53c2e4a7576957822f3851e80a55fada0496a3a9
Trishan <sanjanatrishan@gmail.com>
2025-10-26 00:56:13 +0530

    Refactor product management to use database for CRUD operations and load sample data from JSON

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from fastapi import FastAPI
-from models import Product
+from fastapi import FastAPI, Depends
+from models import ProductBase
 from database import session, engine
 import database_models
-
+from sqlalchemy.orm import Session 
+from pathlib import Path
+import json
 app = FastAPI()
 
 database_models.Base.metadata.create_all(bind=engine)
 
-products : list= [
-    Product(id=1, name="Laptop", description="A high-end laptop", price=1500.00, in_stock=True),
-    Product(id=2, name="Smartphone", description="A latest model smartphone", price=800.00, in_stock=True),
-    Product(id=3, name="Headphones", description="Noise-cancelling headphones", price=200.00, in_stock=False),
-    Product(id=4, name="Monitor", description="4K UHD Monitor", price=400.00, in_stock=True),
-]
+
+sample_data_path =  Path(__file__).with_name("sample_data.json")
+with sample_data_path.open("r", encoding="utf-8") as sample_file:
+    sample_data =json.load(sample_file)  
+
+products = [ProductBase(**item) for item in sample_data]
+
+def get_db():
+    try:
+        db=session()
+        yield db
+    finally:
+        db.close()    
 
 # Initialize database with sample products
 def init_db():
@@ -27,36 +36,42 @@ def init_db():
 init_db()
 
 @app.get("/products")
-def get_all_products():
+def get_all_products(db: Session = Depends(get_db)):
+    db_products = db.query(database_models.Product).all()
     # db = session()
-    return products
+    return db_products
 
 @app.get("/products/{product_id}")
-def get_product_by_id(product_id: int):
-    for product in products:
-        if product.id == product_id:
-            print(f"Product found : {product}")
-            return product
+def get_product_by_id(product_id: int, db:Session = Depends(get_db)):
+    db_product = db.query(database_models.Product).filter(database_models.Product.id==product_id).first()
+    if db_product:
+        return db_product
     return {"error":"Product not found"}
 
 @app.post("/products")
-def add_product(product: Product):
-    products.append(product)
+def add_product(product: ProductBase, db:Session = Depends(get_db)):
+    db.add(database_models.Product(**product.model_dump()))
+    db.commit()
     print(f"New product added : {product}")
     return product
 
-@app.put("/products")
-def update_product(product_id: int, updated_product: Product):
-    for i in range (len(products)):
-        if products[i].id == product_id:
-            products[i] = updated_product
-            return {"message": "Product Updaed Successfully"}
+@app.put("/products/{product_id}")
+def update_product(product_id: int, updated_product: ProductBase, db:Session = Depends(get_db)):
+    db_product = db.query(database_models.Product).filter(database_models.Product.id==product_id).first()
+    if db_product:
+        db_product.name = updated_product.name
+        db_product.description = updated_product.description
+        db_product.price = updated_product.price
+        db_product.in_stock = updated_product.in_stock
+        db.commit()    
+        return {"message": "Product Updaed Successfully"}
     return {"error":"Product not found"}
 
-@app.delete("/products")
-def delete_product(product_id: int):
-    for i in range(len(products)):
-        if products[i].id == product_id:
-            del products[i]
-            return {"message": "Product Deleted Successfully"}
+@app.delete("/products/{product_id}")
+def delete_product(product_id: int, db:Session = Depends(get_db)):
+    db_product = db.query(database_models.Product).filter(database_models.Product.id==product_id).first()
+    if db_product:
+        db.delete(db_product)
+        db.commit()
+        return {"message": "Product Deleted Successfully"}
     return {"error":"Product not found"}
